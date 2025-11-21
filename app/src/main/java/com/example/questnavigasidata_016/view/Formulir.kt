d85e5eba53c0148aee0281b83d11ffcc5a9fa3eb
HafizZada16 <hafizzada16@gmail.com>
2025-11-21 15:37:46 +0700

    Mengubah Pada Form bagian Alamat (2)

--- app/src/main/java/com/example/questnavigasidata_016/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasidata_016/view/Formulir.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -101,9 +102,10 @@ fun FormIsian(
             OutlinedTextField(
                 value = txtAlamat,
                 singleLine = true,
+                shape = MaterialTheme.shapes.medium,
                 modifier = Modifier
                     .width(250.dp),
-                label = {Text(text = "Alamat")},
+                label = {Text(text = "Alamat Lengkap")},
                 onValueChange = {
                     txtAlamat = it
                 },
