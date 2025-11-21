162885432fae5f890eb57f47695c1ec6280477e5
HafizZada16 <hafizzada16@gmail.com>
2025-11-21 15:02:29 +0700

    Add 2 dependencies Implementation lifecycle viewmodel dan runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
