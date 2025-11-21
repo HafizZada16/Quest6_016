6506e1cde326f6f5d6c0a594ea64a5854aa632e9
HafizZada16 <hafizzada16@gmail.com>
2025-11-21 15:13:43 +0700

    Add DataJX dan string nya

---
+++ app/src/main/java/com/example/questnavigasidata_016/model/DataJX.kt
@@ -0,0 +1,11 @@
+package com.example.questnavigasidata_016.model
+
+import com.example.questnavigasidata_016.R
+
+object DataJX {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+
+    )
+}
